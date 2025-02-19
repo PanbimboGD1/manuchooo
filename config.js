c6d4ffed9b0a5d4fb69139761a8eb43769a25440
PanbimboGD <panbimbogd@gmail.com>
2025-02-19 02:47:26 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,25 @@
+
+
+module.exports = {
+  TOKEN: "",
+  language: "es",
+  ownerID: ["1004206704994566164", ""], 
+  mongodbUri : "mongodb+srv://manucho:DCK61p4dYhETb4AsHbVh@manucho.adh7w.mongodb.net/?retryWrites=true&w=majority&appName=manucho",
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "#1db954",
+  activityName: "con las bolas de manucho", 
+  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/8532jdwC3m",
+  embedTimeout: 5, 
+  errorLog: "", 
+  nodes: [
+     {
+      name: "Koi Node V4",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port: 3128,
+      secure: false
+    }
+  ]
+}
