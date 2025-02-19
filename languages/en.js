bc1d85273900cc62425ea80a15509ffafe81931e
PanbimboGD <panbimbogd@gmail.com>
2025-02-19 03:09:28 +0100

    Update en.js

--- languages/en.js
+++ languages/en.js
@@ -1,5 +1,5 @@
 module.exports = {
-    footer: "Developed by SSRR | Prime Music v1.2",
+    footer: "Desarrollado por PanbimboGD | Amo a Manucho v1.0",
     ping: {
       description: "Check the bot latency",
       response: "Pinging...",
@@ -8,7 +8,7 @@ module.exports = {
         responseTime: "- Bot Response Time : **{latency}ms**",
         websocketPing: "- WebSocket Ping : **{ping}ms**",
         uptime: "- Uptime : **{uptime}**",
-        footer: "Developed by SSRR | Prime Music v1.2"
+        footer: "Desarrollado por PanbimboGD | Amo a Manucho v1.0"
       }
     },
     addsong: {
